49a9b06e3b17c3925533de1b2ae7be22de41a935
Harsh Umare <harshumare912@gmail.com>
2025-05-19 22:10:22 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score = "33abc"
+
+// console.log(typeof score); //string hai type
+// console.log(typeof(score)); //string hai type
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); //number hai type
+// console.log(valueInNumber); // NaN 
+
+//number me conversion:-
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = "harsh"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) // true
+
+// conversion to boolean 
+// 1 => true; 0 => false
+// "" => false
+// "harsh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber) // 33
+console.log(typeof stringNumber) // string
+
+
+
+
+
