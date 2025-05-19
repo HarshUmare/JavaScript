d916038a1ddace3fcd51b3df3ed3df60dc3ca97b
Harsh Umare <harshumare912@gmail.com>
2025-05-19 22:29:38 +0000

    conversionOperation commit

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,12 +22,48 @@
 // "" => false
 // "harsh" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber) // 33
-console.log(typeof stringNumber) // string
+// let stringNumber = String(someNumber)
+// console.log(stringNumber) // 33
+// console.log(typeof stringNumber) // string
 
+
+
+// ***********************  Operations *****************************
+
+let value = 3
+let negVal = -value
+// console.log(negVal)
+
+
+let str1 = "hello"
+let str2 = " harsh"
+let str3 = str1 + str2
+// console.log(str3) // hello harsh
+
+// console.log("1" + 2) //12
+// console.log(1 + "2") //12
+// console.log("1" + 2 + 2) //122
+// console.log(1 + 2 + "2") //32
+// console.log(1 + "2" + 2) //122
+
+console.log(true) // true
+console.log(+true) // 1
+console.log(+"") // 0
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCounter++; //101 // ++gameCounter ka ans bhi 101 hi hoga , but not always
+console.log(gameCounter) // 101
+
+// prefix mtlb ++x agr hai, then it returns the value after incrementing
+// postfix mtlb x++ agr hai, then it returns the balue before incrementing
 
 
 
