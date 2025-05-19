f90d46f2b31c1d4c53cff2928fc0c87c7fac6545
Harsh Umare <harshumare912@gmail.com>
2025-05-19 21:57:59 +0000

    datatypes commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treats all JS code as newer version
+// ye compiler k liye instruction hai
+
+//alert(3+3) // we are using Node.js, not browser
+// iska mtlb ye browser me chlega, but nodejs me error dega
+
+
+// number => 2 to the power 53
+// binint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof null) // object hai ye
+console.log(typeof undefined) //undefined hi hai ye
