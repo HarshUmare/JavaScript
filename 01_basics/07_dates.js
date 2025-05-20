f7bd433460ad9079ae87aafe0fd08ca4c0779683
Harsh Umare <harshumare912@gmail.com>
2025-05-20 20:41:11 +0000

    dates in js commit

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,14 @@
+// Dates
+
+let myDate = new Date() 
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+
+let myCreatedDate = new Date(2023, 0, 9)  // isse ye pta chlta hai i month 0 se start hote hai js me
+console.log(myCreatedDate.toDateString())
+
+//jb single digit me count dete hai month ka toh 0 se start hota hai.
+// but jb dd/mm/yyyy k format me dete hai toh month 01 se start hoga
+
+// IMPORTANT:  ctrl + space use krne se saare options bta deta hai vscode
+
